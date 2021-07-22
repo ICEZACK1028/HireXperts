4a49a5e8003ddd31da1f2322bd7316cca97829cd
Jonatan W <josuejonatan142@gmail.com>
2021-07-21 20:29:58 -0600

    Usuario Service updated

--- angular/src/app/components/components.module.ts
+++ angular/src/app/components/components.module.ts
@@ -15,6 +15,7 @@ import { NotificationComponent } from './notification/notification.component';
 import { NgbdModalBasic } from './modal/modal.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
     imports: [
@@ -34,7 +35,8 @@ import { HomeComponent } from './home/home.component';
         NotificationComponent,
         NgbdModalBasic,
         LoginComponent,
-        HomeComponent
+        HomeComponent,
+        PerfilComponent
     ],
     exports:[ ComponentsComponent ]
 })
