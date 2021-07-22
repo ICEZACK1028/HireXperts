283296939671d32004b7d3af55a86693afdc62bc
Gabriel <gfigueros-2016113@kinal.edu.gt>
2021-07-21 18:07:29 -0600

    CRUD especialitys backend

--- node/src/controllers/especialidad.controller.js
+++ node/src/controllers/especialidad.controller.js
@@ -20,6 +20,41 @@ function crearEspecialidad (req, res){
     })
 }
 
+//FUNCION PARA OBTENER ESPECIALIDADES
+
+function obtenerEspecialidades(req, res){
+    especialidadModel.find().populate('especialidad').exec((err, especialidadesEncontradas)=>{
+        if(err) return res.status(500).send({mensaje: 'Error al obtener las especialidades'})
+        return res.status(200).send({especialidadesEncontradas})
+    })
+}
+//FUNCION PARA EDITAR UNA ESPECIALIDAD
+
+function editarEspecialidad(req, res){
+    var params = req.body
+    var idEspecialidad = req.params.idEspecialidad
+
+    especialidadModel.findOneAndUpdate({_id: idEspecialidad}, {nombreEspecialidad: params.nombreEspecialidad}, {new: true, useFindAndModify: false}, (err, especialidadActualizada)=>{
+        if(err) return res.status(500).send({mensaje: 'Error al actualizar la especialidad'})
+        return res.status(200).send({especialidadActualizada})
+    })
+}
+//FUNCION PARA ELIMINAR UNA ESPECIALIDAD
+
+function eliminarEspecialidad(req, res){
+    var idEspecialidad = req.params.idEspecialidad
+
+    especialidadModel.findOneAndDelete({_id: idEspecialidad}, (err, especialidadEncontrada)=>{
+        if(err) return res.status(500).send({mensaje: 'La especialidad no ha podido eliminarse'})
+        return res.status(200).send({mensaje: 'Especialidad eliminada con Exito'})
+    })
+}
+
+
+
 module.exports = {
-    crearEspecialidad
+    crearEspecialidad,
+    obtenerEspecialidades,
+    editarEspecialidad,
+    eliminarEspecialidad
 }
