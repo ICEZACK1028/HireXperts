1d2586f53930836f510c6a10c715d4a8980fca57
Javier Cabrera <jcabrera-2019045@kinal.edu.gt>
2021-07-26 19:46:23 -0600

    Crud Solicitudes

--- node/src/routes/solicitud.routes.js
+++ node/src/routes/solicitud.routes.js
@@ -4,6 +4,9 @@ const express = require('express');
 const solicitudController = require('../controllers/solicitud.controller');
 
 var api = express.Router();
-api.post('/crearSolicitud', solicitudController.crearSolicitud);
+api.post('/crearSolicitud/:contratante', solicitudController.crearSolicitud);
+api.put('/editarSolicitud/:solicitud', solicitudController.editarSolicitud);
+api.delete('/eliminarSolicitud/:solicitud', solicitudController.eliminarSolicitud);
+api.get('/listarSolicitud/:contratante', solicitudController.listarSolicitudes);
 
 module.exports= api;
