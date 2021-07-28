153e9eea2dc769acd8db47dd30d327bc37182627
Jonatan W <josuejonatan142@gmail.com>
2021-07-27 23:00:10 -0600

    Profesional Register In Progress

--- angular/src/app/components/professional-register/professional-register.component.ts
+++ angular/src/app/components/professional-register/professional-register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'app/models/usuario.model';
+import { UsuarioService } from 'app/services/usuario.service';
 
 @Component({
   selector: 'app-professional-register',
   templateUrl: './professional-register.component.html',
-  styleUrls: ['./professional-register.component.scss']
+  styleUrls: ['./professional-register.component.scss'],
+  providers: [UsuarioService]
 })
 export class ProfessionalRegisterComponent implements OnInit {
   data : Date = new Date();
   focus;
   focus1;
-  constructor() { }
+
+  public usuarioModelAdd
+
+
+  constructor(private _usuarioService:UsuarioService) { 
+    this.usuarioModelAdd = new Usuario("","","","","","","","","","","","","",0,"",{nombreProfesion:""},"","",false,0,false)
+  }
 
   ngOnInit() {
     var body = document.getElementsByTagName('body')[0];
@@ -25,5 +34,7 @@ export class ProfessionalRegisterComponent implements OnInit {
     var navbar = document.getElementsByTagName('nav')[0];
     navbar.classList.remove('navbar-transparent');
   }
+
+  
 
 }
