6a9499e3abe0620baf8c325b3436c2f25fccc85c
Javier Cabrera <jcabrera-2019045@kinal.edu.gt>
2021-08-03 00:04:05 -0600

    Eliminacion de Angular

--- angular/src/app/models/especialidad.model.ts
+++
@@ -1,6 +0,0 @@
-export class especialidad{
-    constructor(
-        public _id: String,
-        public nombreEspecialidad: String,
-    ){}
-}
--- angular/src/app/models/profesion.model.ts
+++
@@ -1,7 +0,0 @@
-export class Profesion{
-    constructor(
-        public _id: String,
-        public nombreProfesion: String,
-        public imagen: String,
-    ){}
-}
